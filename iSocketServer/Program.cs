8646e0e0ba7ae64634752e173dff89745ea58506
Kohji Yamamoto <k.yamamoto03@iforcom.jp>
2020-05-25 16:43:34 +0900

    プロジェクト ファイルを追加します。

---
+++ iSocketServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace iSocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
